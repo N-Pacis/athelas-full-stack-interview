3640b4fcb5aeefab1063eb5690049ed39f6cd86f
n-pacis <pacisnkubito@gmail.com>
2022-07-24 21:59:58 +0200

    styled the pages

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.static(path.join(__dirname,"styles")));
 app.set('views',path.join(__dirname,'views'))
 app.set("view engine","ejs")
 app.use(json());
